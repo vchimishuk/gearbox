827c18dc97940849c78497308254674db7ae9b5b
Viacheslav Chimishuk <vchimishuk@yandex.ru>
2025-05-14 00:47:37 +0300

    Add -H command line option.

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 )
 
 const ProgName = "gearbox"
+const Usage = "usage: " + ProgName + " [-H] command [opt]... [arg]..."
 
 type Command interface {
 	Name() string
@@ -19,30 +20,45 @@ type Command interface {
 	Exec(client *transmission.Client, opts opt.Options, args []string) error
 }
 
+var Commands []Command = []Command{
+	NewAddCommand(),
+	NewInfoCommand(),
+	NewListCommand(),
+	NewStatsCommand(),
+	NewTorrentCommand(),
+}
+
 func printErr(err error) {
 	fmt.Fprintf(os.Stderr, "%s: %s\n", ProgName, err)
 }
 
 func usage() {
-	fmt.Fprintf(os.Stderr, "usage: %s command [opt]... [arg]...\n",
-		ProgName)
+	fmt.Fprintf(os.Stderr, "%s\n", Usage)
+}
+
+func help() {
+	fmt.Printf("gearbox is a non-interactive client for transmission-daemon\n")
+	fmt.Printf("\n")
+	fmt.Printf("%s\n", Usage)
+	fmt.Printf("\n")
+	fmt.Printf("commands:\n")
+	for _, c := range Commands {
+		fmt.Printf("  %s\n", c.Usage())
+	}
 }
 
 func main() {
-	commands := []Command{
-		NewAddCommand(),
-		NewInfoCommand(),
-		NewListCommand(),
-		NewStatsCommand(),
-		NewTorrentCommand(),
-	}
-
 	if len(os.Args) < 2 {
 		usage()
 		os.Exit(1)
 	}
 
-	i := slices.IndexFunc(commands, func(c Command) bool {
+	if os.Args[1] == "-H" {
+		help()
+		os.Exit(1)
+	}
+
+	i := slices.IndexFunc(Commands, func(c Command) bool {
 		return c.Name() == os.Args[1]
 	})
 	if i == -1 {
@@ -50,7 +66,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	cmd := commands[i]
+	cmd := Commands[i]
 	cmdOpts := cmd.Options()
 	cmdOpts = append(cmdOpts, &opt.Desc{
 		"h", "", opt.ArgString, "host", "server host name",
